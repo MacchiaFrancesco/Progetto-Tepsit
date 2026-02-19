01c6595ed7c14aa070e57f793d0f87aff76a96d3
Nessunop <lenzininicolo@gmail.com>
2026-02-19 17:17:07 +0100

    Client parser, Client input handler, Server parser, Server input Handler

---
+++ ruota.server/src/main/java/ruota/server/InputHandler.java
@@ -0,0 +1,31 @@
+package ruota.server;
+
+public class InputHandler {
+
+	public static void loginGiocatore(String nome) {
+	}
+
+	public static void inizioPartita(int numeroTurni) {
+	}
+
+	public static void girareRuota() {
+	}
+
+	public static void letteraIndovinata(String lettera) {
+	}
+
+	public static void dareSoluzioneFrase(String frase) {
+	}
+
+	public static void passoTurno() {
+	}
+
+	public static void girareRuotaMeraviglie() {
+	}
+
+	public static void selezioneBusta(int numeroBusta) {
+	}
+
+	public static void controlloConnessione(long timestamp) {
+	}
+}
--- ruota.server/src/main/java/ruota/server/ServerParser.java
+++ ruota.server/src/main/java/ruota/server/ServerParser.java
@@ -2,4 +2,48 @@ package ruota.server;
 
 public class ServerParser {
 
+    public static void parse(String s) {
+        String[] ps = s.split(";");
+
+        switch(Integer.parseInt(ps[0])) {
+            case 000:
+                InputHandler.loginGiocatore(ps[1]);
+                break;
+
+            case 003:
+                InputHandler.inizioPartita(Integer.parseInt(ps[1]));
+                break;
+
+            case 020:
+                InputHandler.girareRuota();
+                break;
+
+            case 030:
+                InputHandler.letteraIndovinata(ps[1]);
+                break;
+
+            case 040:
+                InputHandler.dareSoluzioneFrase(ps[1]);
+                break;
+
+            case 050:
+                InputHandler.passoTurno();
+                break;
+
+            case 101:
+                InputHandler.girareRuotaMeraviglie();
+                break;
+
+            case 104:
+                InputHandler.selezioneBusta(Integer.parseInt(ps[1]));
+                break;
+
+            case 903:
+                InputHandler.controlloConnessione(Long.parseLong(ps[1]));
+                break;
+
+            default:
+                System.out.println("Messaggio non riconosciuto");
+        }
+    }
 }
