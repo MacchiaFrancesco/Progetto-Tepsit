ee421cc909bb18d22f361eb87b3fb49fbcd96e57
Nessunop <lenzininicolo@gmail.com>
2026-02-27 22:11:42 +0100

    Classe Ruota

--- ruota.server/src/main/java/ruota/server/Ruota.java
+++ ruota.server/src/main/java/ruota/server/Ruota.java
@@ -1,5 +1,18 @@
 package ruota.server;
+import java.util.Random;
 
 public class Ruota {
-
+	private int[] ruota = {	200, 350, 100, 200, -1, 300, 150, 400, 250, 500, 
+							-2, 200, 300, 150, 100, 200, -1, 250, 100, 500, 
+							1000, 400, -2, 2000
+							};
+	
+	private Random rand;
+	public Ruota() {
+		this.rand = new Random();
+	}
+	
+	public int giraRuota() {
+		return ruota[rand.nextInt(ruota.length)];
+	}
 }
